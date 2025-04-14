83fa6e47c3f0ec1c33dc567e3a97948ef79575da
Espere-1119-Song <enxin.song.dut@gmail.com>
2025-04-15 00:20:53 +0800

    main

--- website/javascript/benchmark_table.js
+++ website/javascript/benchmark_table.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     minWidth: 50
                 },
                 {
-                    title: "Avg. VDC",
+                    title: "Avg.",
                     field: "avg_acc", 
                     hozAlign: "center", 
                     formatter: colorFormatterAvg, 
